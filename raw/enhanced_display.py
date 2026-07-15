97f36b329f0c7fb23ea11e519041552afcd6c76a
Rob de Vaan <robdevaan@gmail.com>
2026-07-15 15:24:04 +0200

    HTML and Latex line magics added. Shorter line magic names (md, html, ltx).

--- raw/enhanced_display.py
+++ raw/enhanced_display.py
@@ -3,15 +3,25 @@ from IPython.core.magic import Magics, magics_class, line_magic
 
 
 @magics_class
-class EnhancedDisplay(Magics):
+class EnhancedDisplayMagics(Magics):
     @line_magic
-    def markdown(self, line):
-        """Immediately display line content in IPython Markdown form."""
+    def md(self, line):
+        """Immediately display line content in IPython processed as Markdown."""
         ipd.display(ipd.Markdown(line))
+
+    @line_magic
+    def html(self, line):
+        """Immediately display line content in IPython processed as HTML."""
+        ipd.display(ipd.HTML(line))
+
+    @line_magic
+    def ltx(self, line):
+        """Immediately display line content in IPython processed as Latex. Line should not include Latex delimiters (i.e. dollar sign characters)."""
+        ipd.display(ipd.Latex(line))
 
 
 def load_ipython_extension(ipython):
     """
     Register to IPython to allow loading through `%load_ext` or similar commands.
     """
-    ipython.register_magics(EnhancedDisplay)
+    ipython.register_magics(EnhancedDisplayMagics)
